d41f5b8101a49cf8e4e4efc93f89be352dc8f560
Kevin Cosotti <kevin.cosotti@etu.univ-grenoble-alpes.fr>
2020-02-28 15:22:02 +0100

    q10 semi done ( do it if you want more)

--- tp3.php
+++ tp3.php
@@ -1,8 +1,9 @@
 <?php
 require("tp3-helpers.php");
-$filmvo=tmdbget('movie/550');
-$filmva=tmdbget('movie/550',['language'=>'en']);
-$filmvf=tmdbget('movie/550',['language'=>'fr']);
+$id=550;
+$filmvo=tmdbget('movie/'.$id);
+$filmva=tmdbget('movie/'.$id,['language'=>'en']);
+$filmvf=tmdbget('movie/'.$id,['language'=>'fr']);
 $decodevo=json_decode($filmvo,true);
 $decodeva=json_decode($filmva,true);
 $decodevf=json_decode($filmvf,true);
@@ -17,3 +18,7 @@ echo '<tr><th>synopsis</th><td>'.$decodevo['overview']."</td><td>".$decodeva['ov
 echo "</tbody></table>";
 if ($decodevo['homepage']!="")echo "lien: ".'<a href='.$decodevo['homepage'].'>'.$decodevo['homepage'].'</a>';
 else echo "aucun lien trouvé";
+$search=tmdbget('movie/'.$id.'/videos');
+$trailers=json_decode($search,true);
+if(isset($trailers['results'][0])) echo '<br><a href= https://www.youtube.com/watch?v='.$trailers['results'][0]['key'].'>'."trailer du film"."</a>";
+else echo "aucun trailer trouvé";
